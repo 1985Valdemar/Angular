e9bb03391d1f11db226c94127e350de304147e5f
1985Valdemar <vteider@yahoo.com>
2023-10-18 08:40:51 -0300

    Novo4

---
+++ fundamentos-angular/src/app/renderizando-listas/renderizando-listas.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { celular } from '../types/celular';
+
+@Component({
+  selector: 'app-renderizando-listas',
+  templateUrl: './renderizando-listas.component.html',
+  styleUrls: ['./renderizando-listas.component.css']
+})
+export class RenderizandoListasComponent {
+  celulares: celular[] = [
+    {id:1, nome:"celular XL ", descricao:"celular grande ", esgotado:false},
+    {id:2, nome:"celular Normal ", esgotado:false},
+    {id:1, nome:"celular Mini ", descricao:"celular pequeno ", esgotado:true}
+  ]
+
+}
